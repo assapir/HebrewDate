4d01fc406145b3b2052fe2b022de4ad3d4dfd78f
Assaf Sapir <me@ass.af>
2021-09-22 20:55:36 +0300

    Add some more info to package.json

--- index.js
+++
@@ -1,5 +0,0 @@
-const HebrewDate = require('./libhdate-wrapper');
-
-const date = new Date('2021-09-21')
-const hdate = new HebrewDate({ date });
-console.log(hdate.year, hdate.month, hdate.date, hdate.holiday, hdate.holidayName);
